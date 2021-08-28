e9ee57d2296efcf17897a070ff7108013994c9e4
Alex Thanas <athanas1@my.wctc.edu>
2021-08-28 17:33:19 -0500

    Initial commit

---
+++ src/main/java/edu/wctc/salestool/AppConfig.java
@@ -0,0 +1,9 @@
+package edu.wctc.salestool;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("edu.wctc.salestool")
+public class AppConfig {
+}
---
+++ src/main/java/edu/wctc/salestool/SalesTool.java
@@ -0,0 +1,10 @@
+package edu.wctc.salestool;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SalesTool {
+    public SalesTool() {
+        System.out.println("Created");
+    }
+}
